162b521704584c43738083412a3f48f6f1aa3c39
Max Rogers <maxrogers90@gmail.com>
2014-01-28 16:47:23 -0500

    Fixed up the GUI

--- gen/com/SBUMAD/dicemaster/R.java
+++ gen/com/SBUMAD/dicemaster/R.java
@@ -27,14 +27,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ButtonSendFeedback=0x7f090005;
-        public static final int ETNumberOfDice=0x7f090004;
-        public static final int NumberOfDice=0x7f090003;
-        public static final int Result=0x7f090006;
-        public static final int ScrollView01=0x7f090000;
-        public static final int SpinnerFeedbackType=0x7f090002;
-        public static final int TextViewTitle=0x7f090001;
-        public static final int action_settings=0x7f090007;
+        public static final int ButtonSendFeedback=0x7f090004;
+        public static final int ETNumberOfDice=0x7f090003;
+        public static final int NumberOfDice=0x7f090002;
+        public static final int Result=0x7f090005;
+        public static final int SpinnerFeedbackType=0x7f090001;
+        public static final int TextViewTitle=0x7f090000;
+        public static final int action_settings=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
