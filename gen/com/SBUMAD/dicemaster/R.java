a536867a3727f25d0f02e0df8c96e5d6ea83b357
Max Rogers <maxrogers90@gmail.com>
2014-01-28 16:25:30 -0500

    Finished functionality

--- gen/com/SBUMAD/dicemaster/R.java
+++ gen/com/SBUMAD/dicemaster/R.java
@@ -8,6 +8,9 @@
 package com.SBUMAD.dicemaster;
 
 public final class R {
+    public static final class array {
+        public static final int feedbacktypelist=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,18 +27,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int ButtonSendFeedback=0x7f090005;
+        public static final int ETNumberOfDice=0x7f090004;
+        public static final int NumberOfDice=0x7f090003;
+        public static final int Result=0x7f090006;
+        public static final int ScrollView01=0x7f090000;
+        public static final int SpinnerFeedbackType=0x7f090002;
+        public static final int TextViewTitle=0x7f090001;
+        public static final int action_settings=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int feedbackbutton=0x7f050005;
+        public static final int feedbacktitle=0x7f050003;
+        public static final int feedbacktype1=0x7f05000a;
+        public static final int feedbacktype2=0x7f05000b;
+        public static final int feedbacktype3=0x7f05000c;
+        public static final int feedbacktype4=0x7f05000d;
+        public static final int feedbacktype5=0x7f05000e;
+        public static final int feedbacktype6=0x7f05000f;
+        public static final int feedbacktype7=0x7f050010;
         public static final int hello_world=0x7f050002;
+        public static final int numberOfDice=0x7f050006;
+        public static final int numberOfDiceHint=0x7f050007;
+        public static final int numberOfSides=0x7f050008;
+        public static final int result=0x7f050009;
+        public static final int spinnertext=0x7f050004;
     }
     public static final class style {
         /** 
@@ -59,10 +83,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/SBUMAD/dicemaster/MainActivity.java
+++ src/com/SBUMAD/dicemaster/MainActivity.java
@@ -2,7 +2,12 @@ package com.SBUMAD.dicemaster;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +25,25 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void diceRoll(View Button)
+    {
+    	EditText numberOfDice = (EditText) findViewById(R.id.ETNumberOfDice);
+    	String input = numberOfDice.getText().toString();
+    	int iNumberOfDice = Integer.parseInt(input);
+    	Spinner numberOfSides = (Spinner) findViewById(R.id.SpinnerFeedbackType);
+    	String sNumberOfSides = numberOfSides.getSelectedItem().toString();
+    	int iNumberOfSides = Integer.parseInt(sNumberOfSides);
+    	int sum = 0;
+    	for(int i=0;i<iNumberOfDice;i++)
+    	{
+    		sum += rollDice(iNumberOfSides);
+    	}
+    	TextView result = (TextView) findViewById(R.id.Result);
+    	result.setText(""+sum);
+    	Log.i("DiceRoll", ""+sum);
+    }
+    
+    public int rollDice(int numSides){
+    	return (int)(Math.random()*numSides) + 1;
+    }
 }
